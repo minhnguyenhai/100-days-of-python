8b5d5e0ed09af498e6adbaf20ad3098924ac9c51
Nguyen Hai-Minh <151656176+haiminhnguyenn@users.noreply.github.com>
2024-08-02 15:09:44 +0700

    Update main.py

--- Day8/main.py
+++ Day8/main.py
@@ -1,5 +1,3 @@
-from art import logo
-
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
 def caesar(start_text, shift_amount, cipher_direction):
@@ -7,26 +5,36 @@ def caesar(start_text, shift_amount, cipher_direction):
   if cipher_direction == "decode":
     shift_amount *= -1
   for char in start_text:
-    if char.isalpha():
+    if char in alphabet:
       position = alphabet.index(char)
       new_position = position + shift_amount
       end_text += alphabet[new_position]
     else:
       end_text += char
-  print(f"Here's the {cipher_direction}d result: {end_text}\n")
+  print(f"Here's the {cipher_direction}d result: {end_text}")
 
-run = True
-while run:
-  print(logo)
+#TODO-1: Import and print the logo from art.py when the program starts.
+from art import logo
+print(logo)
+
+#TODO-4: Can you figure out a way to ask the user if they want to restart the cipher program?
+#e.g. Type 'yes' if you want to go again. Otherwise type 'no'.
+#If they type 'yes' then ask them for the direction/text/shift again and call the caesar() function again?
+#Hint: Try creating a while loop that continues to execute the program if the user types 'yes'.
+should_end = False
+while not should_end:
   direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
   text = input("Type your message:\n").lower()
   shift = int(input("Type the shift number:\n"))
-
-  if shift > 26:
-    shift = shift % shift == 0
+  #TODO-2: What if the user enters a shift that is greater than the number of letters in the alphabet?
+  #Try running the program and entering a shift number of 45.
+  #Add some code so that the program continues to work even if the user enters a shift number greater than 26. 
+  #Hint: Think about how you can use the modulus (%).
+  shift = shift % 26
 
   caesar(start_text=text, shift_amount=shift, cipher_direction=direction)
-  choice = input("Do you want to run this program again?\nType 'yes' or 'no': ")
-  if choice == 'no':
-    run = False
-    print("Goodbye.")
+
+  restart = input("Type 'yes' if you want to go again. Otherwise type 'no'.\n")
+  if restart == "no":
+    should_end = True
+    print("Goodbye")
