441f5a9aef77775522173e0860b854b6ec07b9d2
Nguyen Hai-Minh <151656176+haiminhnguyenn@users.noreply.github.com>
2024-08-04 23:24:13 +0700

    Create machine_config.py

---
+++ Day015/machine_config.py
@@ -0,0 +1,34 @@
+# Dictionary to store the menu details for the coffee machine
+# Each coffee type has its required ingredients and cost
+MENU = {
+    "espresso": {
+        "ingredients": {
+            "water": 50,
+            "coffee": 18,
+        },
+        "cost": 1.5,
+    },
+    "latte": {
+        "ingredients": {
+            "water": 200,
+            "milk": 150,
+            "coffee": 24,
+        },
+        "cost": 2.5,
+    },
+    "cappuccino": {
+        "ingredients": {
+            "water": 250,
+            "milk": 100,
+            "coffee": 24,
+        },
+        "cost": 3.0,
+    }
+}
+
+# Dictionary to store the current resources available in the coffee machine
+resources = {
+    "water": 300,
+    "milk": 200,
+    "coffee": 100,
+}
